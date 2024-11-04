0ef7be7eb59aa66b64c10c9062114fe661643b82
its-purpol <favre.paul@icloud.com>
2024-11-04 18:43:33 +0100

    Changed the design Added functions to change the score, the opponent's team, and switch the service from team to team

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Happy developing ✨')
+console.log('Hello World!');
+
+let VienneSert = true
+
+function changerScore(equipe, valeur = 1) {
+    let scoreId = "score-" + equipe + "-num";
+    let scoreEl = document.getElementById(scoreId);
+    scoreEl.textContent = (valeur + Number(scoreEl.textContent)).toString();
+}
+
+function definirAdversaire(equipe) {
+    let nomEl = document.getElementById("team-r-name");
+    nomEl.textContent = equipe.toUpperCase();
+}
+
+function changerService() {
+    let serviceEl = document.getElementById('service');
+    if (VienneSert) {
+        serviceEl.style.left = "718px";
+    }
+    else {
+        serviceEl.style.left = "312px";
+    }
+    VienneSert = !VienneSert;
+}
